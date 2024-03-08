7473268c338aa56ddc052c3cab1ce935ae20f5a7
VishnuBhangale <bhangale.vishnu7@gmail.com>
2024-03-08 11:35:24 +0530

    Variable Initialization and Declaration
    Variable Initialization and Declaration

---
+++ 01_Variable.js
@@ -0,0 +1,21 @@
+var city;  //variable Declaration
+city="Pune"; //variable Initialization
+console.log(city);
+console.log("My city name is:", city);
+
+console.log("What is undefined ?");
+var firstName;
+console.log("First Name variable value before Initialization",firstName);
+firstName="Vishnu";
+console.log("First Name variable value after Initialization",firstName);
+
+//override value
+firstName="Ratan";
+console.log("First Name after override",firstName);
+
+console.log("==== Variable Re-declaration ======");
+var pinCode = "411057";
+console.log(pinCode);
+pinCode  = "112233"; // Variable update or override
+
+var pinCode = "445533"; // Variable re-declaration
---
+++ 01_swapVariableAssign.js
@@ -0,0 +1,26 @@
+console.log("==============Assignment2==========");
+console.log("===========STEP 1=============");
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("=======Value Before Swap=======");
+console.log("Sweety:",sweety," Cutie:",cutie);
+console.log("=======Value After Swap=======");
+var temp;
+temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("Sweety:",sweety," Cutie:",cutie);
+
+console.log("===========STEP 2=============");
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("=======Value Before Swap=======");
+console.log("num1:",num1,"num2:",num2,"num3:",num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("=======Value After Swap=======");
+console.log("num1:",num1,"num2:",num2,"num3:",num3);
+
